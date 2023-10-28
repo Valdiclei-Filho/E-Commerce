7cd198b95989acb3f00d173c530fff2bee2e2456
valdiclei.filho <valdiclei.filho@gmail.com>
2023-10-27 21:58:43 -0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div>
+    <h1>E-commerce - VESF</h1>  
+  </div>
+
+</template>
+
+<style scoped>
+
+div{
+  margin-left: auto;
+  margin-right: auto;
+  width: 19em;
+  border-bottom: 1px solid;
+  border-top: 1px solid;
+  border-left: 1px solid;
+  border-right: 1px solid;
+  border-radius: 10px;
+  border-color: black;
+  text-align: center;
+  display: block;
+}
+
+h1 {
+  color: black;
+  padding: 10px;
+  max-width: 282px;
+}
+</style>
---
+++ src/secoes/Menu.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>E-commerce - VESF</h1>
+</template>
+
+<style scoped>
+    h1 {
+        color: black;
+        border: 50px, 50px, black;
+    }
+</style>
